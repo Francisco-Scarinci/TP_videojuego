6ed3145b2114b551b5fc2266eef07a4527d0be04
Francisco-Scarinci <50156336@est.ort.edu.ar>
2026-06-28 16:07:22 -0300

    Segundo commit (TP 14.2)

---
+++ Assets/Scripts/PlayerPickup.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+
+public class PlayerPickup : MonoBehaviour
+{
+    private int contador = 0;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter (Collider other) {
+        if (other.CompareTag ("Pickable")) {
+            contador ++;
+            Debug.Log ("Lápices recolectados: " + contador);
+
+            Destroy (other.gameObject);
+        }
+    }
+}
